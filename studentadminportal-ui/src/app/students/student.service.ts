7ccbcd4b75e360365a1205bdad6be3a0e0edcc3b
alan-jozf <alanpezhumkattil@gmail.com>
2022-06-10 18:57:17 +0530

    End of Section 5

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Student } from '../models/api-models/student.model';
+import { UpadteStudentRequest } from '../models/api-models/update-student-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,19 @@ export class StudentService {
 
   getStudent(studentId: string): Observable<Student>{
     return this.httpClient.get<Student>(this.baseApiUrl + '/students/' + studentId);
+  }
+
+  updateStudent(studentId: string, StudentRequest: Student): Observable<Student> {
+    const updateStudentRequest: UpadteStudentRequest = {
+      firstName: StudentRequest.firstName,
+      lastName: StudentRequest.lastName,
+      dateOfBirth: StudentRequest.dateOfBirth,
+      email: StudentRequest.email,
+      mobile: StudentRequest.mobile,
+      genderId: StudentRequest.genderId,
+      physicalAddress: StudentRequest.address.physicalAddress,
+      postalAddress: StudentRequest.address.postalAddress
+    }
+    return this.httpClient.put<Student>(this.baseApiUrl + '/students/' + studentId, updateStudentRequest);
   }
 }
